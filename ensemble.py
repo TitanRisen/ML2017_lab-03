a6a356f6b55f7cbb3628f1a9e73de5cdc2624c4a
TitanRisen <thomas9@126.com>
2017-12-19 18:51:58 +0800

    Add files via upload

---
+++ ensemble.py
@@ -0,0 +1,143 @@
+import pickle
+
+from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+from numpy import *
+class AdaBoostClassifier:
+    '''A simple AdaBoost Classifier.'''
+
+    def __init__(self, weak_classifier, n_weakers_limit):
+        '''Initialize AdaBoostClassifier
+        Args:
+            weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
+            n_weakers_limit: The maximum number of weak classifier the model can use.
+        '''
+        self.weak_classifier = weak_classifier
+        self.n_weakers_limit = n_weakers_limit
+       
+        #存放训练好的弱分类器和alpha
+        self.weak_classifier_list = []
+        self.alpha_list = []
+
+    def is_good_enough(self):
+        '''Optional'''
+        pass
+
+    def fit(self,X,y):
+        '''Build a boosted classifier from the training set (X, y).
+
+        Returns:
+            X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
+            y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
+        '''
+        m,n = X.shape
+        Classifier = self.weak_classifier
+        errorRate_list=[]
+        #initialize D (weights)
+        D = array(ones(m))
+        D = D * (1/m)
+        #最终估计分类用矩阵
+        totalClassEst = array(zeros(m))
+        for i in range(self.n_weakers_limit):
+            #fit every weak Classifier and add its prediction to the list
+            #use just 1 floor
+            self.weak_classifier_list.append(Classifier(max_depth = 1 ))
+            self.weak_classifier_list[i].fit(X,y,sample_weight = D)
+            pred = self.weak_classifier_list[i].predict(X)
+            # 1^-1=-2 , 1^1 = 0 , -1^-1 = 0
+            #error_num = -(pred ^ y).sum()/2
+            error_num = (pred != y).sum()
+            #culcate the error rate
+            error = error_num / m
+            print(i," the sub error rate is ",error)
+            alpha = float(0.5*log((1.0-error)/max(error, 1e-16)))
+            # if predict is correct, then *exp(-alpha) to make the weights smaller ,else * exp(alpha)
+            expon = multiply(-1*alpha*y, pred)
+            D = multiply(D,exp(expon))
+            D = D/D.sum()
+            
+            #add the alpha to the class Container
+            self.alpha_list.append(alpha)
+            
+            #Calculate the total error of the training set
+            totalClassEst += alpha * pred
+            ClassEst = sign(totalClassEst)
+            ClassEst_errors = (ClassEst != y)
+            total_error_rate = ClassEst_errors.sum()/m
+            print(" the total error rate is ",total_error_rate)
+            errorRate_list.append(total_error_rate) 
+            #if the training model has no error ,it can stop          
+            if total_error_rate == 0:break
+        
+        #For convenience , I save the training model
+        self.save(self.alpha_list , "./alpha_list.pkl")
+        self.save(self.weak_classifier_list,"./weak_classifier_list.pkl")
+        print("train success")
+        #self.draw_curve(errorRate_list)
+    
+
+    def draw_curve(self,errorRate_list):
+        iter_list = []
+        for i in range(len(errorRate_list)):
+            iter_list.append(i)
+        plt.figure()
+
+        plt.plot(iter_list,errorRate_list,linestyle = '-.',color = 'black',linewidth = 2.0, label = 'test')
+        plt.xlabel('iter_times')  
+        plt.ylabel('error_rate')
+        plt.legend()
+        plt.show()
+
+    def predict_scores(self, X):
+        '''Calculate the weighted sum score of the whole base classifiers for given samples.
+
+        Args:
+            X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
+
+        Returns:
+            An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
+        '''
+        pass
+
+    def predict(self, X, y,threshold=0):
+        '''Predict the catagories for geven samples.
+
+        Args:
+            X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
+            threshold: The demarcation number of deviding the samples into two parts.
+
+        Returns:
+            An ndarray consists of predicted labels, which shape should be (n_samples,1).
+        '''
+        #load the model which has been trained
+        weak_classifier_list = self.load("./weak_classifier_list.pkl")
+        alpha_list = self.load('./alpha_list.pkl')
+        m,n = X.shape
+        ClassEst = 0
+        totalClassEst = array(zeros(m))
+        errorRate_list = []
+        for i in range(self.n_weakers_limit):
+            #using the weak classifier and alpha stored.
+            pred = weak_classifier_list[i].predict(X)
+            totalClassEst += alpha_list[i] * pred
+            ClassEst = sign(totalClassEst)
+            ClassEst_errors = (ClassEst != y)
+            total_error_rate = ClassEst_errors.sum()/m
+            print(i," the total error rate in predicting the validation set is ",total_error_rate)
+            errorRate_list.append(total_error_rate)
+            #if the training model has 99% correct rate ,it can stop 
+            if total_error_rate <= 0.01:break
+        self.draw_curve(errorRate_list)
+        return ClassEst
+
+    @staticmethod
+    def save(model, filename):
+        with open(filename, "wb+") as f:
+            pickle.dump(model, f)
+
+    @staticmethod
+    def load(filename):
+        with open(filename, "rb") as f:
+            return pickle.load(f)
---
+++ train.py
@@ -0,0 +1,99 @@
+import os
+from ensemble import *
+import pickle
+from PIL import Image
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+from feature import *
+import numpy as np
+def load_image(FACE,max_num = 200):
+    featurelist = []
+    #如果找到了已生成的pkl文件就返回
+    if FACE == True:
+        if os.path.exists("./face.pkl"):
+            with open("./face.pkl" , "rb+") as f:
+                featurelist = pickle.load(f)
+            return featurelist
+    elif FACE == False:
+        if os.path.exists("./nonface.pkl"):
+            with open("./nonface.pkl" , "rb+") as fn:
+                featurelist = pickle.load(fn)
+            return featurelist
+    
+    if FACE == True:
+        dirPath =  "./datasets/original/face/"
+    else:
+        dirPath =  "./datasets/original/nonface/"
+    
+    dirlist = os.listdir(dirPath)
+    count = 0
+    for file in dirlist:
+        if count < max_num:
+            img = Image.open(dirPath + file )
+            img = img.resize((24,24))
+            img = img.convert('L')
+            img_array = np.array(img)
+            NPD = NPDFeature(img_array)
+            NPD_feature = NPD.extract()
+            featurelist.append(NPD_feature)
+            count += 1
+            print(count,NPD_feature.shape)
+    
+    if FACE == True:
+        faceFile = open("face.pkl","wb")
+        pickle.dump(featurelist,faceFile)
+    else:
+        nonfaceFile = open("nonface.pkl","wb")
+        pickle.dump(featurelist,nonfaceFile )
+    return featurelist
+
+if __name__ == "__main__":
+    # write your code here
+    
+    size = 500
+    faceList = load_image(True , max_num = size)
+    nonfaceList = load_image(False,max_num =size)
+
+    labelList = [1] * size + [-1] * size
+    X = faceList + nonfaceList
+    print(len(X))
+    X_train, X_test, y_train, y_test = train_test_split(X, labelList, test_size=0.25)
+    X_train = mat(X_train)
+    X_test = mat(X_test)
+    y_train = array(y_train)
+    y_test = array(y_test)
+    print(X_train.shape,y_train.shape)
+
+    adaBoostClassifier = AdaBoostClassifier(weak_classifier = DecisionTreeClassifier ,
+                                        n_weakers_limit = 25)
+    #adaBoostClassifier.fit(X_train,y_train)
+    pred = adaBoostClassifier.predict(X_test,y_test)
+    #write the report.txt
+    target_names = ['nonface', 'face'] 
+    report = classification_report(y_test, pred, target_names=target_names)
+    print(report)
+    with open("./report.txt","w") as f:
+        f.write(report)
+
+    '''
+    m,n = X_train.shape
+    #initialize D (weights)
+    D = array(ones(m))
+    D = D * (1/m)
+    print(D.shape)
+    clf = DecisionTreeClassifier(max_depth=2)
+    a = 1/150
+    a = clf.fit(X_train,y_train,sample_weight = D)
+    b = clf.predict(X_test)
+    c = b^y_test
+    print(type(clf.copy()))
+    #print(c.sum())
+    #print(c)
+    '''
+    '''
+    a = array([-1,-1,-1])
+    b = array([1,-1,1])
+    target_names = ['class 0', 'class 1']
+    print(classification_report(a, b, target_names=target_names))
+    '''
